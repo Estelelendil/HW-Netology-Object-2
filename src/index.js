358d2e884da3bf6cb45199781e988d2a4fa1c66d
Чернова Александра <estelelendil@mail.ru>
2022-07-28 13:02:31 +0300

    Function destructor is working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import './css/style.css';
+import specialDestructor from './js/basic';
 
-// TODO: write your code in app.js
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+console.log(specialDestructor(character));
