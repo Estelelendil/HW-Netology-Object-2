facca821b32060bd93e5fd79185d683d9e8d942d
Чернова Александра <estelelendil@mail.ru>
2022-07-28 20:07:46 +0300

    added test, 100% covarage

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,43 @@
 // import sum from '../basic';
+import specialDestructor from '../basic';
 
-// test('should sum', () => {
-//   const result = sum([1, 2, 3]);
+test('testing specialDestructor function in standart case', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
 
-//   expect(result).toBe(6);
-// });
+  expect(result).toEqual(specialDestructor(character));
+});
